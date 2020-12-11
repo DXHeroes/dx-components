f25f87b2067788aed95249e0de49309bf75c8e4b
Prokop Simek <prokop@dxheroes.io>
2020-12-11 19:42:22 +0100

    chore: init repo

---
+++ src/components/Button.tsx
@@ -0,0 +1,8 @@
+import { Button as AntButton } from 'antd';
+import React from "react"
+
+// export const Button: typeof AntButton = (props) => {
+//   return (
+//     <AntButton {...props}></AntButton>
+//   )
+// }
